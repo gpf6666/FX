6d117d503964de9d46e6b7281fcadb5a4e3aa771
stark.guo <stark.guo@zoom.us>
2025-07-30 14:50:19 +0800

    Updates

--- assets/_plugin-vue_export-helper.js
+++ assets/_plugin-vue_export-helper.js
@@ -1,1 +1,1 @@
-const s=(t,r)=>{const o=t.__vccOpts||t;for(const[c,e]of r)o[c]=e;return o};export{s as _};
+var _=Object.defineProperty;var c=(t,o)=>_(t,"name",{value:o,configurable:!0});const a=c((t,o)=>{const r=t.__vccOpts||t;for(const[e,s]of o)r[e]=s;return r},"_export_sfc");export{a as _};
